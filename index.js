ff8e43115f56757afabaf3e630a9b363fa876961
Racostadev <[200~racostawebdev@gmail.com>
2023-11-14 22:52:11 +0100

    Calculadora_JS

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const botones = document.querySelectorAll(".button");
-const operator = document.querySelectorAll(".operador");
-
-function mostrar(valor) {
-  let panta = document.getElementById("pantalla").textContent;
-  panta += valor;
-
-  document.scrollingElement("pantalla").textContent = pantalla;
+//funcion donde muestra en pantalla//
+function mostrar(valor){
+  document.getElementById("pantalla").value +=valor;
 }
 
+//funcion que borra la pantalla//
+
+function borrar(){
+  document.getElementById('pantalla').value = '';
+}
+
+//Calculos Suma-Resta-Multiplicacion-División//
+
+function calcular(){
+  let pantaresult = document.getElementById('pantalla').value
+  let total = eval(pantaresult)
+  document.getElementById('pantalla').value = total
+}
